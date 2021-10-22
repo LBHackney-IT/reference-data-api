0dc0e800f5b9e8ec7b0d2a33c607a188112309c5
Charnjit Sohal <charnjit.sohal@news.co.uk>
2021-10-22 09:46:25 +0100

    added CW dashboard

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -8,16 +8,16 @@
 # 7) ENSURE THIS FILE IS PLACED WITHIN A 'terraform' FOLDER LOCATED AT THE ROOT PROJECT DIRECTORY
 
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 3.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 data "aws_caller_identity" "current" {}
@@ -25,8 +25,8 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-    parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
-    esDomain = "https://${module.elasticsearch_db_development.es_endpoint_url}"
+  parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
+  esDomain        = "https://${module.elasticsearch_db_development.es_endpoint_url}"
 }
 
 terraform {
@@ -55,28 +55,35 @@ data "aws_subnet_ids" "development" {
 }
 
 module "elasticsearch_db_development" {
-  source           = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/database/elasticsearch"
-  vpc_id           = data.aws_vpc.development_vpc.id
-  environment_name = "development"
-  port             = 443
-  domain_name      = "reference-data-api-es"
-  subnet_ids       = [tolist(data.aws_subnet_ids.development.ids)[0]]
-  project_name     = "reference-data-api"
-  es_version       = "7.8"
-  encrypt_at_rest  = "false"
-  instance_type    = "t3.small.elasticsearch"
-  instance_count   = "2"
-  ebs_enabled      = "true"
-  ebs_volume_size  = "30"
-  region           = data.aws_region.current.name
-  account_id       = data.aws_caller_identity.current.account_id
-  create_service_role   = false
+  source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/database/elasticsearch"
+  vpc_id              = data.aws_vpc.development_vpc.id
+  environment_name    = "development"
+  port                = 443
+  domain_name         = "reference-data-api-es"
+  subnet_ids          = [tolist(data.aws_subnet_ids.development.ids)[0]]
+  project_name        = "reference-data-api"
+  es_version          = "7.8"
+  encrypt_at_rest     = "false"
+  instance_type       = "t3.small.elasticsearch"
+  instance_count      = "2"
+  ebs_enabled         = "true"
+  ebs_volume_size     = "30"
+  region              = data.aws_region.current.name
+  account_id          = data.aws_caller_identity.current.account_id
+  create_service_role = false
 }
 
 resource "aws_ssm_parameter" "reference_data_elasticsearch_domain" {
-  name = "/reference-data-api/development/elasticsearch-domain"
-  type = "String"
+  name  = "/reference-data-api/development/elasticsearch-domain"
+  type  = "String"
   value = local.esDomain
 }
 
-
+module "reference_data_api_cloudwatch_dashboard" {
+  source                  = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudwatch/dashboards/api-dashboard"
+  environment_name        = var.environment_name
+  api_name                = "reference-data-api"
+  include_sns_widget      = false
+  include_dynamodb_widget = false
+  no_sns_widget_dashboard = false
+}
