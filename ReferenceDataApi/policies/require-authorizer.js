9b57704029e757ce78d860a06e8bb0b8e653964b
LBHMGeorgieva <mirela.georgieva@hackney.gov.uk>
2022-02-23 11:51:27 +0000

    add missing and

--- ReferenceDataApi/policies/require-authorizer.js
+++ ReferenceDataApi/policies/require-authorizer.js
@@ -14,7 +14,7 @@ module.exports = function authorizerPolicy(policy, service) {
 
     functionConfig.events.forEach((eventConfig) => {
       if (!eventConfig.http) return;
-      if (!eventConfig.http.authorizer & !eventConfig.http.path=="/swagger/{proxy+}") {
+      if (!eventConfig.http.authorizer && !eventConfig.http.path=="/swagger/{proxy+}") {
 		    failed = true;
 			policy.fail(
 			`Function "${functionName}" does not have an authorizer attached to it. `
